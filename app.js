8f7bb63b2a1bd7c1e08841180376a4b41e2091a0
iananiev <ivanananievuk@yahoo.com>
2023-12-15 00:07:26 +0000

    Verification email

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const Router = require('./routes/main');
 //const MailingFile = require('./routes/mailingfile');
@@ -7,6 +8,7 @@ const Router = require('./routes/main');
 const cookies = require('cookie-parser');
 const userModel = require('./models/usermodel');
 const currentUser = 0;
+let theCode;
 const jwt = require('jsonwebtoken');
 const {auth} = require('./Middleware/middAuth');
 
@@ -67,13 +69,62 @@ app.get('/register',(req, res) => res.render('register'));
 app.post('/register', async (req, res) => {
 
 
-  setInterval(function() {
-    console.log("Hello, World!");
-}, 5000); 
+  function runForFiveMinutes() {                //This generates a random 6 digit code and deletes i after 5 min
+
+const generateRandomNumber = function() {                             //the part that is creating the number or the act function
+  
+    const randomNumber = Math.floor(Math.random() * 1000000);         //It generate a number between 0&1 and x1000000
+    const formattedNumber = randomNumber.toString().padStart(6, '0');  //it takes only the first 6 digits and convers then to a string
+    theCode = formattedNumber;                                         // setting it to the global var so the other functions can access it 
+    console.log("Generated Number:", theCode);      //you can delete this line of code it's for demonstration
+};
+generateRandomNumber();
 
 
-   //res.render("/verify");
-   const data = req.body;
+   
+    
+    setTimeout(function() {                            //creating a timing bomb so the global var wil expire
+        theCode = 0;   
+        console.log("Session has expired");
+    }, 300000); // 300,000 milliseconds = 5 minutes
+}
+
+runForFiveMinutes();
+
+// This is where we actually send the email 
+
+const html = `
+Your verification code is ${theCode}.
+`;
+
+
+async function mainMail() {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mail.yahoo.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'ivanananievuk@yahoo.com',
+      pass: 'brcvijbdpwqyymvb'
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: 'Ivan <ivanananievuk@yahoo.com>',
+    to: 'iananiev83@gmail.com',
+    subject: 'Verification Code',
+    html: html,
+  });
+
+  console.log('Message sent:', info.messageId);
+}
+
+mainMail();
+
+
+
+
+   const data = req.body;               //if everything is correct
    res.render('verify',  data );
 
    
@@ -81,7 +132,17 @@ app.post('/register', async (req, res) => {
 });
 
 app.get('/verify',(req, res) => res.render('verify'));
-app.post('/verify', (req,res)=>{res.send(req.body)})
+
+//app.post('/verify', (req,res)=>{res.send(req.body)})
+app.post('/verify', (req,res)=>{
+  console.log(req.body)
+  if (req.body.code == theCode) {
+    res.send('The code is the same we can create the account')
+  } else {
+    res.send('Incorrect code')
+
+  }
+})
 
 
 
